e8d05e3b88f594def21a50c0b79c1c6a787d6b1d
souamisantana <mlenasantanasantos@gmail.com>
2022-11-08 01:32:59 -0300

    Primeiro Commit

---
+++ js/script.js
@@ -0,0 +1,65 @@
+const addBtn = document.querySelector('#add');
+const minusBtn = document.querySelector('#minus');
+const divideBtn = document.querySelector('#divide');
+const multiplyBtn = document.querySelector('#multiply');
+const n1 = document.querySelector('#n1');
+const n2 = document.querySelector('#n2');
+const h2 = document.querySelector('h2');
+const deleteAll = document.querySelector('.delete button');
+
+let result;
+
+function validateNumber() {
+    if (!n1.valueAsNumber && !n2.valueAsNumber) {
+        alert(`Preencha os dois números!`);
+        return false;
+    }
+    return true;
+}
+
+addBtn.addEventListener('click', (event) => {
+    event.stopPropagation();
+    if (!validateNumber()) {
+        return;
+    }
+    result = n1.valueAsNumber + n2.valueAsNumber;
+    h2.textContent = `Resultado: ${result}`;
+});
+
+minusBtn.addEventListener('click', (event) => {
+    event.stopPropagation();
+    if (!validateNumber()) {
+        return;
+    }
+    result = n1.valueAsNumber - n2.valueAsNumber;
+    h2.textContent = `Resultado: ${result}`;
+});
+
+divideBtn.addEventListener('click', (event) => {
+    event.stopPropagation();
+    if (!validateNumber()) {
+        return;
+    }
+
+    if (n2.valueAsNumber !== 0) {
+        result = n1.valueAsNumber / n2.valueAsNumber;
+        h2.textContent = `Resultado: ${result}`;
+    } else {
+        alert(`Não é possível dividir por zero!`);
+    }
+    
+});
+
+multiplyBtn.addEventListener('click', (event) => {
+    if (!validateNumber()) {
+        return;
+    }
+    result = n1.valueAsNumber * n2.valueAsNumber;
+    h2.textContent = `Resultado: ${result}`;
+});
+
+deleteAll.addEventListener('click', () => {
+    n1.value = '';
+    n2.value = '';
+    h2.textContent = `Resultado:`
+});
